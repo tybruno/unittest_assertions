5cf19a5b8b5d6d72c7ce435a5a4913fb91320afb
tybruno <tybruno@cisco.com>
2021-09-03 11:30:05 -0700

    added TestAssertEqual and TestAssertNotEqual

--- tests/unit/test_comparison.py
+++ tests/unit/test_comparison.py
@@ -1,3 +1,5 @@
+from abc import abstractmethod
+
 from unittest_assertions.comparison import (
     EqualityAssertion,
     AssertEqual,
@@ -6,40 +8,55 @@ from unittest_assertions.comparison import (
     AssertNotAlmostEqual,
     AssertCountEqual,
 )
-from unittest import TestCase
+
+from tests.conftest import (
+    combined_equal_all_basic_types,
+    combined_non_equal_all_basic_types,
+)
 import pytest
 
 
 class TestAssertEqual:
     @pytest.mark.parametrize(
         "equal_data",
-        (
-            (1, 1),
-            (-1.2, -1.2),
-            ("string", "string"),
-            (["list", 1], ["list", 1]),
-        ),
+        tuple(combined_equal_all_basic_types()),
     )
     @pytest.mark.parametrize("msg", ("message", None, 2))
     def test_assert_equal_passes(self, msg, equal_data):
-
         assert_equal = AssertEqual(msg=msg)
         assert assert_equal.msg == msg
         assert_equal(*equal_data)
 
     @pytest.mark.parametrize(
         "unequal_data",
-        (
-            (1, 2),
-            (-1.2, -1.29),
-            ("string", "string2"),
-            (["list", 1], ["list2", 2]),
-        ),
+        tuple(combined_non_equal_all_basic_types()),
     )
     @pytest.mark.parametrize("msg", ("message", None, 2))
     def test_assert_equal_raises_assertion(self, msg, unequal_data):
-
         assert_equal = AssertEqual(msg=msg)
         assert assert_equal.msg == msg
         with pytest.raises(AssertionError):
             assert_equal(*unequal_data)
+
+
+class TestAssertNotEqual:
+    @pytest.mark.parametrize(
+        "unequal_data",
+        tuple(combined_non_equal_all_basic_types()),
+    )
+    @pytest.mark.parametrize("msg", ("message", None, 2))
+    def test_assert_equal_passes(self, msg, unequal_data):
+        assert_not_equal = AssertNotEqual(msg=msg)
+        assert assert_not_equal.msg == msg
+        assert_not_equal(*unequal_data)
+
+    @pytest.mark.parametrize(
+        "equal_data",
+        tuple(combined_equal_all_basic_types()),
+    )
+    @pytest.mark.parametrize("msg", ("message", None, 2))
+    def test_assert_equal_raises_assertion(self, msg, equal_data):
+        assert_not_equal = AssertNotEqual(msg=msg)
+        assert assert_not_equal.msg == msg
+        with pytest.raises(AssertionError):
+            assert_not_equal(*equal_data)
