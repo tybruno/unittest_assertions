8dcaba0626562b59b296dd0edd96ad9cf7de1e97
tybruno <tybruno@cisco.com>
2021-09-08 08:52:43 -0700

    Completed TestAssertTrue and TestAssertFalse

--- tests/unit/test_logic.py
+++ tests/unit/test_logic.py
@@ -11,10 +11,22 @@ class TestAssertTrue(AssertionTester):
         "testing_data", tuple(value for value in ALL_BASIC_TYPES_1.values())
     )
     def test_assertion_passes(self, testing_data):
-        print(testing_data)
         super().test_assertion_passes(testing_data)
 
-    # @pytest.mark.parametrize("testing_data", (tuple(False), 0, None, "", []))
-    # def test_assertion_raises(self, testing_data):
-    #     print(testing_data)
-    #     super().test_assertion_raises(testing_data)
+    @pytest.mark.parametrize("testing_data", (False, 0, None, "", []))
+    def test_assertion_raises(self, testing_data):
+        super().test_assertion_raises(testing_data)
+
+
+class TestAssertFalse(AssertionTester):
+    _assertion = AssertFalse
+
+    @pytest.mark.parametrize("testing_data", (False, 0, None, "", []))
+    def test_assertion_passes(self, testing_data):
+        super().test_assertion_passes(testing_data)
+
+    @pytest.mark.parametrize(
+        "testing_data", tuple(value for value in ALL_BASIC_TYPES_1.values())
+    )
+    def test_assertion_raises(self, testing_data):
+        super().test_assertion_raises(testing_data)
