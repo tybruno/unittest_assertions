0d627a3fda830c0da03994992d751ed0c6e751ce
tybruno <tybruno@cisco.com>
2021-09-09 09:51:24 -0700

    refactoring

--- tests/base.py
+++ tests/base.py
@@ -1,10 +1,24 @@
 from typing import Type
 import pytest
-from unittest_assertions.base import BuiltinAssertion
+from unittest_assertions.base import AbstractAssertion, BasicBuiltinAssertion
+from abc import abstractmethod, ABC
+
+
+class BasicAsssertionTester:
+    _assertion: BasicBuiltinAssertion
+
+    def test_assertion_passes(self, *args, **kwargs):
+        assertion = self._assertion()
+        assertion(*args, **kwargs)
+
+    def test_assertion_raises(self, *args, **kwargs):
+        assertion = self._assertion()
+        with pytest.raises(AssertionError):
+            assertion(*args, **kwargs)
 
 
 class AssertionTester:
-    _assertion: BuiltinAssertion
+    _assertion: AbstractAssertion
 
     @pytest.mark.parametrize("msg", ("message", None, 2))
     def test_init(self, msg):
--- tests/unit/test_control.py
+++ tests/unit/test_control.py
@@ -1,20 +1,13 @@
 import pytest
-from tests.base import AssertionTester
+from tests.base import BasicAsssertionTester
 from unittest_assertions.control import AssertRaises, AssertWarns, AssertLogs
-
-
-def raiser(message=None):
-    def _raise():
-        raise Exception(message)
-
-    return _raise()
 
 
 def _raise(e):
     raise e
 
 
-class TestAssertRaises(AssertionTester):
+class TestAssertRaises(BasicAsssertionTester):
     _assertion = AssertRaises
 
     @pytest.mark.parametrize(
@@ -22,6 +15,41 @@ class TestAssertRaises(AssertionTester):
         (
             (KeyError, _raise, KeyError),
             (KeyError, _raise, KeyError("key")),
+        ),
+    )
+    def test_assertion_passes(self, testing_data: tuple):
+        super().test_assertion_passes(*testing_data)
+
+    @pytest.mark.parametrize(
+        "testing_data",
+        (
+            (
+                KeyError,
+                lambda: None,
+            ),
+        ),
+    )
+    def test_assertion_raises(self, testing_data: tuple):
+        super().test_assertion_raises(*testing_data)
+
+    @pytest.mark.parametrize(
+        "testing_data",
+        ((KeyError, _raise, ValueError),),
+    )
+    def test_assertion_error(self, testing_data: tuple):
+        assert_raises = self._assertion()
+        with pytest.raises(testing_data[2]):
+            assert_raises(*testing_data)
+
+
+class TestAssertWarns(BasicAsssertionTester):
+    _assertion = AssertWarns
+
+    @pytest.mark.parametrize(
+        "testing_data",
+        (
+            (Warning, _raise, Warning),
+            (Warning, _raise, Warning("Warning")),
         ),
     )
     def test_assertion_passes(self, testing_data: tuple):
