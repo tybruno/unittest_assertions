401cb2272a6c62b3cb4f734b4891343e3bada1b4
tybruno <tybruno@cisco.com>
2021-09-09 13:42:08 -0700

    Fixing AssertWarns

--- tests/unit/test_control.py
+++ tests/unit/test_control.py
@@ -1,3 +1,6 @@
+import warnings
+import logging
+
 import pytest
 from tests.base import BasicAsssertionTester
 from unittest_assertions.control import AssertRaises, AssertWarns, AssertLogs
@@ -5,6 +8,10 @@ from unittest_assertions.control import AssertRaises, AssertWarns, AssertLogs
 
 def _raise(e):
     raise e
+
+
+def _warning(message, warning: Warning):
+    warnings.warn(message, warning)
 
 
 class TestAssertRaises(BasicAsssertionTester):
@@ -48,8 +55,8 @@ class TestAssertWarns(BasicAsssertionTester):
     @pytest.mark.parametrize(
         "testing_data",
         (
-            (Warning, _raise, Warning),
-            (Warning, _raise, Warning("Warning")),
+            (Warning, _warning, "message", Warning),
+            (Warning, _warning, "message", UserWarning),
         ),
     )
     def test_assertion_passes(self, testing_data: tuple):
@@ -58,20 +65,9 @@ class TestAssertWarns(BasicAsssertionTester):
     @pytest.mark.parametrize(
         "testing_data",
         (
-            (
-                KeyError,
-                lambda: None,
-            ),
+            (UserWarning, _warning, "message", SyntaxWarning),
+            (UserWarning, _warning, "message", Warning),
         ),
     )
     def test_assertion_raises(self, testing_data: tuple):
         super().test_assertion_raises(*testing_data)
-
-    @pytest.mark.parametrize(
-        "testing_data",
-        ((KeyError, _raise, ValueError),),
-    )
-    def test_assertion_error(self, testing_data: tuple):
-        assert_raises = self._assertion()
-        with pytest.raises(testing_data[2]):
-            assert_raises(*testing_data)
