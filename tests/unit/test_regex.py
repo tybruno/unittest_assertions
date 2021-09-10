9195be7e4e9572df60bb03244909ebc3899516b2
tybruno <tybruno@cisco.com>
2021-09-10 14:17:19 -0700

    add TestAssertRegex and TestAssertNotRegex

--- tests/unit/test_regex.py
+++ tests/unit/test_regex.py
@@ -5,6 +5,7 @@ from unittest_assertions.regex import (
     AssertWarnsRegex,
     AssertRegex,
     AssertNotRegex,
+    AssertRegex,
 )
 
 
@@ -28,3 +29,37 @@ def _raise(e):
 #     )
 #     def test_assertion_raises(self, testing_data: tuple):
 #         super().test_assertion_raises(*testing_data)
+class TestAssertRegex(BasicAsssertionTester):
+    _assertion = AssertRegex
+
+    @pytest.mark.parametrize(
+        "testing_data",
+        (("Ala ma kota", r"k.t"),),
+    )
+    def test_assertion_passes(self, testing_data):
+        super().test_assertion_passes(*testing_data)
+
+    @pytest.mark.parametrize(
+        "testing_data",
+        (("Ala ma kota", r"r+"),),
+    )
+    def test_assertion_raises(self, testing_data: tuple):
+        super().test_assertion_raises(*testing_data)
+
+
+class TestAssertNotRegex(BasicAsssertionTester):
+    _assertion = AssertNotRegex
+
+    @pytest.mark.parametrize(
+        "testing_data",
+        (("Ala ma kota", r"r+"),),
+    )
+    def test_assertion_passes(self, testing_data):
+        super().test_assertion_passes(*testing_data)
+
+    @pytest.mark.parametrize(
+        "testing_data",
+        (("Ala ma kota", r"k.t"),),
+    )
+    def test_assertion_raises(self, testing_data: tuple):
+        super().test_assertion_raises(*testing_data)
