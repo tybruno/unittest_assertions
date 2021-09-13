caa6752ba6ab20dfe9e0109e0ec92086ebdaa45f
tybruno <tybruno@cisco.com>
2021-09-13 08:52:09 -0700

    adding documentation

--- unittest_assertions/base.py
+++ unittest_assertions/base.py
@@ -2,7 +2,7 @@ from dataclasses import (
     dataclass,
     field,
 )
-from typing import Callable, Any, Optional, Mapping, Iterable
+from typing import Callable, Any, Optional, Mapping, Sequence
 from abc import abstractmethod, ABC
 from string import Template
 
@@ -11,12 +11,12 @@ class AbstractAssertion(ABC):
     """Foundational Abstract Method for Assertions"""
 
     @abstractmethod
-    def __call__(self, *args: Iterable, **kwargs: Mapping) -> None:
+    def __call__(self, *args: Sequence, **kwargs: Mapping) -> None:
         """Method to run the assertion.
 
         Args:
-            *args: Arguments to be ran on the Assertion function
-            **kwargs: Keyword arguments to be ran on the Assertion Function
+            *args: Arguments to be passed to the Assertion function
+            **kwargs: Keyword arguments to be passed to the Assertion Function
 
         Returns:
             None
@@ -26,14 +26,14 @@ class AbstractAssertion(ABC):
 
 @dataclass
 class BasicBuiltinAssertion(AbstractAssertion):
-    """Basic Builtin Assertion
+    """Basic Builtin Assertion base class
 
     `_assertion_function` is the assertion method from the `unittest` library.
     """
 
     _assertion_function: Callable
 
-    def __call__(self, *args: Iterable, **kwargs: Mapping) -> None:
+    def __call__(self, *args: Sequence, **kwargs: Mapping) -> None:
         """Run the Assertion function with the given args and kwargs.
 
         Args:
@@ -48,12 +48,26 @@ class BasicBuiltinAssertion(AbstractAssertion):
 
 @dataclass
 class BuiltinAssertion(BasicBuiltinAssertion):
+    """Buitlin Assertion base class for assertions that have a `msg` argument"""
+
     msg: Optional[Any] = field(default=None)
 
-    def __call__(self, *args, **kwargs):
+    def __call__(self, *args: Sequence, **kwargs: Mapping) -> None:
+        """Run the Assertion function with the given args and kwargs
+
+        Args:
+            *args: Arguments that will be passed to the `_assertion_function`
+            **kwargs: Keyword arguments to be passed to the `_assertion_function`
+
+        Returns:
+            None
+        """
         if "msg" not in kwargs:
-            msg = self.msg
+            msg: Any = self.msg
+
+            # If it is a Template string substitute it with the kwargs
             if isinstance(msg, Template):
-                msg = msg.safe_substitute(kwargs)
+                msg: str = msg.safe_substitute(kwargs)
             kwargs["msg"] = msg
+
         super().__call__(*args, **kwargs)
--- unittest_assertions/comparison.py
+++ unittest_assertions/comparison.py
@@ -1,17 +1,48 @@
-from unittest_assertions.base import BuiltinAssertion
-from dataclasses import dataclass, field
-from typing import Callable, Any, Sequence
+from dataclasses import (
+    dataclass,
+    field,
+)
+from typing import Any, Callable, Sequence, Mapping, Union
 from unittest import TestCase
+
+from unittest_assertions.base import BuiltinAssertion
 
 
 @dataclass
 class EqualityAssertion(BuiltinAssertion):
-    def __call__(self, first: Any, second: Any, **kwargs):
-        super().__call__(first=first, second=second, **kwargs)
+    """Parent class for Equality Assertions"""
+
+    def __call__(
+        self, first: Any, second: Any, *args: Sequence, **kwargs: Mapping
+    ) -> None:
+        """Run the `_assertion_function`.
+
+        Args:
+            first: First item that will be passed to the `_assertion_function`
+            second: Second item that will be passed to the `_assertion_function`
+            *args: Arguments to be passed to the Assertion function
+            **kwargs: Keyword arguments to be passed to the Assertion Function
+
+        Returns:
+            None
+        """
+        super().__call__(first=first, second=second, *args, **kwargs)
 
 
 @dataclass
 class AssertEqual(EqualityAssertion):
+    """assert `first` `==` `second`
+
+    raise `AssertionError` if `first` is not equal to `second`
+
+    For more documentation read TestCase().assertEqual.__doc__
+
+    Example:
+        >>> assert_equal = AssertEqual()
+        >>> assert_equal(1,1)
+        >>> assert_equal(first="hello",second="hello")
+    """
+
     _assertion_function: Callable = field(
         default=TestCase().assertEqual, init=False
     )
@@ -19,6 +50,18 @@ class AssertEqual(EqualityAssertion):
 
 @dataclass
 class AssertNotEqual(EqualityAssertion):
+    """assert `first` `!=` `second`
+
+    raise `AssertionError` if `first` is equal to `second`
+
+    For more documentation read TestCase().assertNotEqual.__doc__
+
+    Example:
+        >>> assert_equal = AssertNotEqual()
+        >>> assert_equal(1,2)
+        >>> assert_equal(first="hello",second="goodbye")
+    """
+
     _assertion_function: Callable = field(
         default=TestCase().assertNotEqual, init=False
     )
@@ -26,11 +69,42 @@ class AssertNotEqual(EqualityAssertion):
 
 @dataclass
 class AssertAlmostEqual(EqualityAssertion):
+    """assert `first` `~=` `second`
+
+    raise `AssertionError` if `first` is not almost equal to `second`
+
+    For more documentation read TestCase().assertAlmostEqual.__doc__
+
+    Example:
+        >>> assert_almost_equal = AssertAlmostEqual()
+        >>> assert_almost_equal(1.00000001, 1.0)
+        >>> assert_almost_equal(first=1.1, second=1.0, float=None, delta=0.5)
+    """
+
     _assertion_function: Callable = field(
         default=TestCase().assertAlmostEqual, init=False
     )
 
-    def __call__(self, first: Any, second: Any, places=None, delta=None):
+    def __call__(
+        self,
+        first: Any,
+        second: Any,
+        places: Any = None,
+        delta: Any = None,
+    ) -> None:
+        """assert `first` almost equals `second`
+
+        Args:
+            first: object that will be compared with second
+            second: object that will be compared with second
+            places: The difference rounded to the given decimal places
+            (default: 7 decimal places)
+            delta: The amount of differnece that will raise the
+            `AssertionError`
+
+        Returns:
+            None
+        """
         super().__call__(
             first=first, second=second, places=places, delta=delta
         )
@@ -38,6 +112,18 @@ class AssertAlmostEqual(EqualityAssertion):
 
 @dataclass
 class AssertNotAlmostEqual(AssertAlmostEqual):
+    """assert `first` `!~=` `second`
+
+    raise `AssertionError` if `first` is almost  equal to `second`
+
+    For more documentation read TestCase().assertNotAlmostEqual.__doc__
+
+    Example:
+        >>> assert_not_almost_equal = AssertNotAlmostEqual()
+        >>> assert_not_almost_equal(1.00000001, 2.0)
+        >>> assert_not_almost_equal(first=1.1,second= 1.0, places=None, delta=0.05)
+    """
+
     _assertion_function: Callable = field(
         default=TestCase().assertNotAlmostEqual, init=False
     )
