7685b5ef6f525a4acf7d46c1a199738accc29638
tybruno <tybruno@cisco.com>
2021-09-13 07:49:54 -0700

    Refactoring

--- tests/unit/test_regex.py
+++ tests/unit/test_regex.py
@@ -3,7 +3,6 @@ from tests.base import BasicAsssertionTester
 from unittest_assertions.regex import (
     AssertRaisesRegex,
     AssertWarnsRegex,
-    AssertRegex,
     AssertNotRegex,
     AssertRegex,
 )
@@ -13,22 +12,6 @@ def _raise(e):
     raise e
 
 
-# class TestAssertRaisesRegex(BasicAsssertionTester):
-#     _assertion = AssertRaisesRegex
-#
-#     @pytest.mark.parametrize(
-#         "testing_data",
-#         (),
-#     )
-#     def test_assertion_passes(self, testing_data):
-#         super().test_assertion_passes(*testing_data)
-#
-#     @pytest.mark.parametrize(
-#         "testing_data",
-#         ((ValueError, r"k.t", ValueError("hello"), ""),),
-#     )
-#     def test_assertion_raises(self, testing_data: tuple):
-#         super().test_assertion_raises(*testing_data)
 class TestAssertRegex(BasicAsssertionTester):
     _assertion = AssertRegex
 
